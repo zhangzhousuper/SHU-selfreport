007d73f1c506c22555b7885e8c6217e751bfd007
zhangzhousuper <52350596+zhangzhousuper@users.noreply.github.com>
2021-01-18 06:36:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fState import F_STATE_GENERATOR
 import base64
 import re
 
-NEED_BEFORE = True  # 如需补报则置为True，否则False
+NEED_BEFORE = false  # 如需补报则置为True，否则False
 START_DT = dt.datetime(2021, 01, 01)  # 需要补报的起始日期
 XIAOQU = "宝山"  # 宝山、嘉定或延长
 
